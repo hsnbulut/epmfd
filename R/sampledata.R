074d0844130fb176b28452ccff98cb7a442d13e7
Assoc. Prof. Hasan Bulut <hasan.bulut@omu.edu.tr>
2025-10-04 01:00:35 +0300

    Release v1.0.3 – sync all files & docs

---
+++ R/sampledata.R
@@ -0,0 +1,13 @@
+#' Example Polytomous Response Data
+#'
+#' A small toy dataset included in the **epmfd** package, containing
+#' polytomous item responses from simulated persons.
+#'
+#' @format A data frame with 20 persons (rows) and 6 items (columns).
+#'   Each item takes ordered values 1–5.
+#'
+#' @examples
+#' data(sampledata)
+#' head(sampledata)
+#'
+"sampledata"
